26aaf1bff40af63e12f3365cbdf3b03f2ad19573
ashanr <ashanrajapaksha@live.com>
2018-09-17 15:02:33 +0530

    system footer

--- include/systemFooter.php
+++ include/systemFooter.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <!-- Required Framework JavaScript -->
 <script src="js/libs/jquery-3.3.1.min.js"></script>
 <!--<script src="js/libs/jquery-3.3.1.min.map"></script>-->
@@ -11,7 +11,6 @@
 <script src="js/theme/navigation.js" defer></script><!-- Header Navigation JS Plugin -->
 <script src="js/theme/jquery.flexslider-min.js" defer></script><!-- FlexSlider JS Plugin -->		
 <script src="js/theme/script.js" defer></script><!-- Custom Document Ready JS -->
-=======
 
 
 <!--CUSTOM JS FILES-->
@@ -21,7 +20,6 @@
 <!-- THEME JS-->
 <!--<script src="js/vendor.js"></script>-->
 <!--<script src="js/app.js"></script>-->
->>>>>>> 856ca2bf50bce58d263f98f1502ef6023f8ad9e3
 
 <!--ALERTIFY-->
 <!--<script type="text/javascript" src="js/alertify/alertify.js"></script>-->
@@ -30,26 +28,23 @@
 <!-- AJAX CROSS ORIGIN -->
 <script type="text/javascript" src="js/jquery.ajax-cross-origin.min.js"></script>
 
-<<<<<<< HEAD
-=======
+
 <!--ALERTIFY-->
 <script type="text/javascript" src="js/popper/popper.min.js"></script>
 <script type="text/javascript" src="js/popper/popper.min.js.map"></script>
 
->>>>>>> 856ca2bf50bce58d263f98f1502ef6023f8ad9e3
 <!--CUSTOM JS FILES-->
 <script type="text/javascript" src="js/systemConfig.js"></script>
 <script type="text/javascript" src="js/userManegmantConfig.js"></script>
 <script type="text/javascript" src="js/loadTables.js"></script>
 <script type="text/javascript" src="js/loadComboBox.js"></script>
 
-<<<<<<< HEAD
-=======
+
 
 <!--DATEPICKER-->
 <script type="text/javascript" src="./js/custom/datepicker.js"></script>
 
->>>>>>> 856ca2bf50bce58d263f98f1502ef6023f8ad9e3
+
 <!--
 Designed and Developed By CyberCodeITSolutions                   
 -->
