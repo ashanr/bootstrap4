a043fb0a27ae4a7c617dac6c54ef7514c3673ba3
ashanr <ashanrajapaksha@live.com>
2018-09-17 15:07:38 +0530

    footer modified

--- include/Footer.php
+++ include/Footer.php
@@ -14,20 +14,3 @@
         </ul>
     </div>
 </footer>
-<footer id="footer-pro">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md">
-                        <div class="copyright-text-pro">&copy; Copyright 2018 . All Rights Reserved</div>
-                    </div><!-- close .col -->
-                    <div class="col-md">
-                        <ul class="social-icons-pro">
-                            <li class="facebook-color"><a href="http://facebook.com/" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
-                            <li class="twitter-color"><a href="http://twitter.com/" target="_blank"><i class="fab fa-twitter"></i></a></li>
-                            <li class="youtube-color"><a href="http://youtube.com" target="_blank"><i class="fab fa-youtube"></i></a></li>
-                            <li class="vimeo-color"><a href="http://vimeo.com" target="_blank"><i class="fab fa-vimeo-v"></i></a></li>
-                        </ul>
-                    </div><!-- close .col -->
-                </div><!-- close .row -->
-            </div><!-- close .container -->
-        </footer>
